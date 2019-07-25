fdf8bb5835da3a6be41df1d0a0f8bc60bda4de8a
renukeswarchinta <41563424+renukeswarchinta@users.noreply.github.com>
2019-07-25 11:15:50 +0530

    First Commit

---
+++ owner-service/src/test/java/com/ledger/owner/ownerservice/OwnerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ledger.owner.ownerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OwnerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
